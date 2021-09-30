bd580eef9ee8a135d6292e059342be593d651e81
kotsukaku <65598274+kotsukaku@users.noreply.github.com>
2021-09-30 21:39:15 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.markdown('### 三次元測定機 :sunglasses:')
-x = st.slider('数値を選んでください')
+x = st.slider('数値を選んでください'min_value=0, max_value=1000)
 st.write(x, 'の三次元の値は：', x**3)
 st.markdown('> Streamlitは便利です :+1:')
